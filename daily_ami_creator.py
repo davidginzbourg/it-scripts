0f18fefc58895785ef582b770ca71b07c19eb547
David Ginzbourg <davidginzbourg@gmail.com>
2016-12-31 17:20:59 +0200

    Implement deregister AMIs

--- daily_ami_creator.py
+++ daily_ami_creator.py
@@ -1,21 +1,6 @@
 import boto3
 
 
-def create_ami(client, instance_id, ami_name):
-    """Creates an AMI from the given instance
-
-    :param client: AWS boto3 client
-    :param instance_id: EC2 instance ID
-    :param ami_name: Desired AMI name
-    :param profile_name: AWS credentials local profile name
-    """
-
-    return client.create_image(
-        InstanceId=instance_id,
-        Name=ami_name,
-        NoReboot=True)
-
-
 def deregister_old_amis(client, ami_prefix, expiration):
     """Deregisters old amis that are older than the given expiration days.
 
@@ -24,12 +9,22 @@ def deregister_old_amis(client, ami_prefix, expiration):
     which in the creation time (in unix time) appears. I.e.
     '<ami_prefix><unix_time>'
     :param expiration: Expiration time in seconds
+    :return: Old amis imade IDs
     """
 
+    old_amis_id = []
 
     ami_list = client.describe_images(
-        ImageIds=['*' + ami_prefix + '*']
+        ImageIds=[ami_prefix + '*']
     )
+
+    for ami in ami_list['Images']:
+        if is_ami_expired(ami['ImageId'], expiration):
+            old_amis_id.append(ami['ImageId'])
+            client.deregisger_image(ImageId=old_amis_id)
+
+    return old_amis_id
+
 
 def get_client(profile_name):
     """Creates an AWS boto3 client
@@ -41,14 +36,23 @@ def get_client(profile_name):
     return session.client('ec2')
 
 
+def is_ami_expired(ami_name, expiration):
+    """Check if the AMI has expired
+
+    :param ami_name: The AMI name
+    :param expiration: Expiration time in seconds
+    :return: True if expired, False
+    """
+
+    pass
+
+
+def daily_run():
+    """Script to be run daily
+    """
+
+# client.create_image(
+#     InstanceId=instance_id,
+#     Name=ami_name,
+#     NoReboot=True)
 # create_ami('i-d689e86b', 'david_test_1', 'default')
-
-"""
-
-Filter [
-    {
-        Name: image-id
-        Values: ['image-id']
-        }
-]
-"""
