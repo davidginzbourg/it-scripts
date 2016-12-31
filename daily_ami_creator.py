f0a9896a3998cd8d55fc8ee7e765b1224d9b4128
David Ginzbourg <davidginzbourg@gmail.com>
2016-12-31 17:58:32 +0200

    Add old amis' snapshots

--- daily_ami_creator.py
+++ daily_ami_creator.py
@@ -15,7 +15,7 @@ def deregister_old_amis(client, ami_prefix, expiration):
     :return: Old amis imade IDs
     """
 
-    old_amis_id = []
+    old_amis_ids = []
 
     ami_list = client.describe_images(
         ImageIds=[ami_prefix + '*']
@@ -23,10 +23,10 @@ def deregister_old_amis(client, ami_prefix, expiration):
 
     for ami in ami_list['Images']:
         if is_ami_expired(ami['ImageId'], expiration):
-            old_amis_id.append(ami['ImageId'])
-            client.deregisger_image(ImageId=old_amis_id)
+            old_amis_ids.append(ami['ImageId'])
+            client.deregisger_image(ImageId=old_amis_ids)
 
-    return old_amis_id
+    return old_amis_ids
 
 
 def get_client(aws_access_key_id, aws_secret_access_key):
@@ -52,16 +52,22 @@ def is_ami_expired(ami_name, expiration):
     pass
 
 
-def gen_ami_name(ami_name):
+def gen_ami_name(ami_name_prefix):
     """Generates AMI name
 
-    :param ami_name: The AMI name, used as a prefix
+    :param ami_name_prefix: The AMI name, used as a prefix
     :return: Generated AMI name
     """
     timestamp = int(time.time())
 
-    return ami_name + str(timestamp)
+    return ami_name_prefix + str(timestamp)
 
+def delete_old_snapshots(ami_ids):
+    """Deletes old snapshots given an ami id
+
+    :param ami_id: AMI IDs list
+    """
+    pass
 
 def daily_run():
     """Script to be run daily
@@ -69,7 +75,7 @@ def daily_run():
 
     # Expiration time in seconds
     expiration = os.environ.get('EXPIRATION')
-    ami_name = os.environ.get('AMI_NAME')
+    ami_name_prefix = os.environ.get('AMI_PREFIX')
     instance_id = os.environ.get('INSTANCE_ID')
 
     aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -77,14 +83,16 @@ def daily_run():
 
     client = get_client(aws_access_key_id, aws_secret_access_key)
 
-    generated_ami_name = gen_ami_name(ami_name)
+    generated_ami_name = gen_ami_name(ami_name_prefix)
 
     # Register AMI
     client.create_image(
         InstanceId=instance_id,
         Name=generated_ami_name,
         NoReboot=True)
-    
+
+    # Delete old AMIs
+    old_ami_ids = deregister_old_amis(client, ami_name_prefix, expiration)
 
 
 # create_ami('i-d689e86b', 'david_test_1', 'default')
