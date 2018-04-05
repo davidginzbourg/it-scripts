35c219f421eb60a1ff1137eda82522e65f4df52f
David Ginzbourg <davidginzbourg@gmail.com>
2018-04-05 12:40:12 -0700

    Fix memset and mail

--- openstack-reports/memset_and_mail.py
+++ openstack-reports/memset_and_mail.py
@@ -10,7 +10,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def send_mail(msg):
+def send_mail(msg, source_email_address, source_email_address_password,
+              destination_email_address, **kwargs):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(source_email_address, source_email_address_password)
@@ -42,19 +43,16 @@ def set_environ_vars(config_file):
         conf_vars = yaml.load(config.read())
     for key, value in conf_vars['ENV_VARS'].items():
         os.environ[key] = value
-    flavor_count = conf_vars['flavors']
-    destination_email_address = conf_vars['destination_email_address']
-    source_email_address = conf_vars['source_email_address']
-    source_email_address_password = conf_vars['source_email_address_password']
-    global flavor_count
-    global metric_suffix
-    global destination_email_address
-    global source_email_address
-    global source_email_address_password
-
+    return {
+        'flavor_count': conf_vars['flavors'],
+        'destination_email_address': conf_vars['destination_email_address'],
+        'source_email_address': conf_vars['source_email_address'],
+        'source_email_address_password': conf_vars[
+            'source_email_address_password']
+    }
 
 def get_data(config_file):
-    set_environ_vars(config_file)
+    global_vars = set_environ_vars(config_file)
     projects_names = get_project_list()
     tenants_instances = {}
     total_instances = 0
@@ -70,15 +68,16 @@ def get_data(config_file):
                 tenants_instances[project['Name']] += 1
                 instances_count += 1
                 flavor = instance['Flavor Name']
-                if flavor in flavor_count:
-                    flavor_count[flavor] += 1
+                if flavor in global_vars['flavor_count']:
+                    global_vars['flavor_count'][flavor] += 1
                 else:
-                    flavor_count[flavor] = 1
+                    global_vars['flavor_count'][flavor] = 1
     tenants_instances['Total'] = total_instances
-    return flavor_count, tenants_instances
+    return tenants_instances, global_vars
 
 
-def build_msg(flavor_count, tenants_instances):
+def build_msg(flavor_count, tenants_instances, source_email_address,
+              destination_email_address, **kwargs):
     project_name_dict = {
         'tenant_name': 'long name'
     }
@@ -139,8 +138,9 @@ def build_msg(flavor_count, tenants_instances):
 def main():
     filename = os.path.basename(__file__).split('.')[0]
     config_file = '{0}.yaml'.format(filename)
-    flavor_count, tenants_instances = get_data(config_file)
-    send_mail(build_msg(flavor_count, tenants_instances))
+    tenants_instances, global_vars = get_data(config_file)
+    send_mail(build_msg(tenants_instances=tenants_instances, **global_vars),
+              **global_vars)
 
 
 if __name__ == '__main__':
