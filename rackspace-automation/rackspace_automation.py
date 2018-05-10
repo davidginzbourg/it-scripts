727cfcf86142763da004abf720c5fefbb418c3ce
David Ginzbourg <davidginzbourg@gmail.com>
2018-05-10 17:24:18 +0300

    Add create action for to running transition

--- rackspace-automation/rackspace_automation.py
+++ rackspace-automation/rackspace_automation.py
@@ -232,7 +232,8 @@ def get_transition(action_str):
     :return: a transition.
     :rtype: StateTransition
     """
-    to_running = {'rebuild', 'resume', 'os-start', 'unpause', 'unshelve'}
+    to_running = {'create', 'rebuild', 'resume', 'os-start', 'unpause',
+                  'unshelve'}
     to_shelved = {'shelve', 'shelveOffload'}
     to_stopped = {'pause', 'os-stop', 'suspend'}
     if action_str in to_running:
