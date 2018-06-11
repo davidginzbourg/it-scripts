cf0bf2657fcece2c6ddfd6430274eaebd35ee673
David Ginzbourg <davidginzbourg@gmail.com>
2018-06-11 15:03:21 +0300

    Add a test setup.py

---
+++ rackspace-automation/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='rackspace_automation', version='1.0', packages=find_packages(),
+      py_modules=['rackspace_automation'],
+      install_requires=[
+          "python-keystoneclient==3.16.0",
+          "python-novaclient==10.2.0",
+          "oauth2client==4.1.2",
+          "gspread==3.0.0",
+          "boto3==1.7.12"
+      ])
