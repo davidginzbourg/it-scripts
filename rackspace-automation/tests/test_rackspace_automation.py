8030f1270f91175b1f8274a61083126cfffac54f
David Ginzbourg <davidginzbourg@gmail.com>
2018-06-14 15:27:26 +0300

    Update second dummy test file

--- rackspace-automation/tests/test_rackspace_automation.py
+++ rackspace-automation/tests/test_rackspace_automation.py
@@ -32,6 +32,9 @@ send_warnings
 shelve
 '''
 
+glb_exc_class = rackspace_automation.RackspaceAutomationException
+
+
 class OsEnvVarsTest(unittest.TestCase):
     """Tests os environment variables check.
     """
@@ -42,40 +45,40 @@ class OsEnvVarsTest(unittest.TestCase):
         os_environ_dict = {}
         magic_mock = MagicMock()
         magic_mock.mock.dict('os.environ', os_environ_dict)
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['SOURCE_EMAIL_ADDRESS'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['CREDENTIALS_FILE_PATH'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['SPREADSHEET_ID'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['SETTINGS_WORKSHEET'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['EMAIL_ADDRESSES_WORKSHEET'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['INSTANCE_SETTINGS_WORKSHEET'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['OPENSTACK_MAIN_PROJECT'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['OPENSTACK_URL'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['OPENSTACK_USERNAME'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['OPENSTACK_PASSWORD'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
         os_environ_dict['DEFAULT_NOTIFICATION_EMAIL_ADDRESS'] = 'something'
-        self.assertRaises(rackspace_automation.RackspaceAutomationException,
+        self.assertRaises(glb_exc_class,
                           rackspace_automation.check_os_environ_vars)
 
 
@@ -129,18 +132,134 @@ class FetchConfigurationsTests(unittest.TestCase):
                          some_email_addr,
                          'Email Addresses value is incorrect')
 
-    @mock.patch('os.environ', os_environ)
-    @mock.patch('gspread.authorize')
-    def test_fetch_email_addresses(self, mock_gspread_authorize):
+    @mock.patch('rackspace_automation.get_worksheet_contents')
+    def test_fetch_email_addresses(self, mock_contents):
         """Tests the fetch_email_addresses function.
         """
-        contents = {}
-        mock_gclient = MagicMock()
-        mock_sheet = MagicMock()
-        mock_worksheet = MagicMock()
-        mock_gspread_authorize.return_value = mock_gclient
-        mock_gclient.open_by_key.return_value = mock_sheet
-        mock_sheet.worksheet.return_value = mock_worksheet
-        mock_worksheet.get_all_records.return_value = contents
+        contents = []
+        mock_contents.return_value = contents
 
-        # self.ass
+        self.assertRaises(glb_exc_class,
+                          rackspace_automation.fetch_email_addresses, None)
+
+        contents.extend([{'random_key': 'random_val'}])
+        self.assertRaises(glb_exc_class,
+                          rackspace_automation.fetch_email_addresses, None)
+        del contents[:]
+
+        tenant1 = 'tenant1'
+        tenant2 = 'tenant2'
+        email1 = 'email1'
+        email2 = 'email2'
+        contents.extend([{rackspace_automation.TENANT_NAME: tenant1,
+                          rackspace_automation.EMAIL_ADDRESS: email1},
+                         {rackspace_automation.TENANT_NAME: tenant2,
+                          rackspace_automation.EMAIL_ADDRESS: email2}])
+        result = rackspace_automation.fetch_email_addresses(None)
+        self.assertIn(tenant1, result, "tenant1 doesn't appear in the result")
+        self.assertIn(tenant2, result, "tenant2 doesn't appear in the result")
+        self.assertEqual(result[tenant1], email1, 'first email address was '
+                                                  'not found')
+        self.assertEqual(result[tenant2], email2, 'first email address was '
+                                                  'not found')
+
+    @mock.patch('rackspace_automation.TimeThresholdSettings')
+    @mock.patch('rackspace_automation.get_worksheet_contents')
+    def test_fetch_global_settings(self, mock_contents, mock_ttsettings):
+        """Tests the fetch_global_settings function.
+        """
+        contents = []
+        mock_contents.return_value = contents
+        self.assertRaises(glb_exc_class,
+                          rackspace_automation.fetch_global_settings, None)
+
+        contents.extend([{'random_key': 'random_val'}])
+        self.assertRaises(glb_exc_class,
+                          rackspace_automation.fetch_global_settings, None)
+        del contents[:]
+
+        val1 = 'val1'
+        val2 = 'val2'
+        val3 = 'val3'
+        val4 = 'val4'
+        val5 = 'val5'
+        val6 = 'val6'
+        contents.extend([{'shelve_running_warning_threshold': val1,
+                          'shelve_stopped_warning_threshold': val2,
+                          'delete_warning_threshold': val3,
+                          'shelve_running_threshold': val4,
+                          'shelve_stopped_threshold': val5,
+                          'delete_shelved_threshold': val6}])
+        rackspace_automation.fetch_global_settings(None)
+        mock_ttsettings.assert_called_with(**contents[0])
+
+    @mock.patch('rackspace_automation.get_worksheet_contents')
+    def test_fetch_instance_settings(self, mock_contents):
+        """Tests fetch_instance_settings.
+        """
+        contents = []
+        mock_contents.return_value = contents
+
+        self.assertFalse(rackspace_automation.fetch_instance_settings(None),
+                         "Fetch instance settings returned a non-empty dict.")
+
+        inst1 = 'inst1'
+        inst_set1 = {rackspace_automation.INSTANCE_NAME: inst1,
+                     'shelve_running_warning_threshold':
+                         '',
+                     'shelve_stopped_warning_threshold':
+                         1.0,
+                     'delete_warning_threshold': 2.0,
+                     'shelve_running_threshold': 3.0,
+                     'shelve_stopped_threshold': 4.0,
+                     'delete_shelved_threshold': 5.0}
+        inst2 = 'inst2'
+        inst_set2 = {rackspace_automation.INSTANCE_NAME: inst2,
+                     'shelve_running_warning_threshold':
+                         10.0,
+                     'shelve_stopped_warning_threshold':
+                         11.0,
+                     'delete_warning_threshold': 12.0,
+                     'shelve_running_threshold': 13.0,
+                     'shelve_stopped_threshold': 14.0,
+                     'delete_shelved_threshold': 15.0}
+        inst3 = 'inst3'
+        inst_set3 = {rackspace_automation.INSTANCE_NAME: inst3,
+                     'shelve_running_warning_threshold':
+                         110.0,
+                     'shelve_stopped_warning_threshold':
+                         111.0,
+                     'delete_warning_threshold': 112.0,
+                     'shelve_running_threshold': 113.0,
+                     'shelve_stopped_threshold': 114.0,
+                     'delete_shelved_threshold': 115.0}
+        project1 = 'project1'
+        project2 = 'project2'
+        row1 = {rackspace_automation.PROJECT_NAME: project1}
+        row1.update(inst_set1)
+        row2 = {rackspace_automation.PROJECT_NAME: project2}
+        row2.update(inst_set2)
+        row3 = {rackspace_automation.PROJECT_NAME: project1}
+        row3.update(inst_set3)
+        contents.extend([row1, row2, row3])
+
+        result = rackspace_automation.fetch_instance_settings(None)
+
+        # This must be done in order to successfully do the double
+        # 'dereference'
+        for inst in [inst_set1, inst_set2, inst_set3]:
+            del inst[rackspace_automation.INSTANCE_NAME]
+
+        inst_set1['shelve_running_warning_threshold'] = float('inf')
+        correct_result = {
+            project1: {
+                inst1: rackspace_automation.TimeThresholdSettings(**inst_set1),
+                inst3: rackspace_automation.TimeThresholdSettings(**inst_set3)
+            },
+            project2: {
+                inst2: rackspace_automation.TimeThresholdSettings(**inst_set2)
+            }
+        }
+
+        self.assertDictEqual(result, correct_result, "Result dict isn't "
+                                                     "correct.")
