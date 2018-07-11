077b3e72aded1954ca7d1be31fb55697e2cdedd4
David Ginzbourg <davidginzbourg@gmail.com>
2018-07-11 11:13:43 +0300

    Fix formatting curly brackets

--- rackspace-automation/html_formats.py
+++ rackspace-automation/html_formats.py
@@ -2,27 +2,27 @@ msg_html = """
     <html>
     <head>
         <style>
-            table,p {
+            table,p {{
                 font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
                 border-collapse: collapse;
                 width: 100%;
-            }
+            }}
 
-            table td, #customers th {
+            table td, #customers th {{
                 border: 1px solid #ddd;
                 padding: 8px;
-            }
+            }}
 
-            table tr:nth-child(even){background-color: #eeeeee;}
+            table tr:nth-child(even){{background-color: #eeeeee;}}
 
-            table tr:hover {background-color: #ddd;}
+            table tr:hover {{background-color: #ddd;}}
 
-            table th {
+            table th {{
                 padding: 8px;
                 text-align: left;
                 background-color: #c6c6c6;
                 color: black;
-            }
+            }}
         </style>
     </head>
     <body>
