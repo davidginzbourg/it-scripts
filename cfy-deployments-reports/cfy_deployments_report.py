76d270b2f5d3c70fbcfb8d0aa907313c534b9fd2
David Ginzbourg <davidginzbourg@gmail.com>
2018-08-29 18:28:24 -0700

    Fix script

--- cfy-deployments-reports/cfy_deployments_report.py
+++ cfy-deployments-reports/cfy_deployments_report.py
@@ -6,6 +6,17 @@ from cloudify_rest_client import CloudifyClient
 import html_formats as h_format
 
 
+def list(api_func):
+    offset = 999
+    response = api_func(_size=offset)
+    result = response.items
+    total = response.metadata.pagination.total
+    for cur_offset in range(offset, total, offset):
+        response = api_func(_size=offset, _offset=cur_offset)
+        result.extend(response.items)
+    return result
+
+
 def main(event, context):
     manager_url = os.environ.get('MANAGER_URL')
     username = os.environ.get('USERNAME')
@@ -29,24 +40,31 @@ def main(event, context):
                             username=username,
                             password=password,
                             tenant='default_tenant')
-    deployments = sorted(client.deployments.list(),
-                         key=lambda x: x['updated_at'],
-                         reverse=True)
 
+    instances = list(client.node_instances.list)
+    deployments = dict(map(
+        lambda x: (x.id, x), list(client.deployments.list)))
     table_cells = ""
     msg = h_format.p.format("Here are the current active deployments:")
-    for depl in deployments:
-        ID = depl.id
-        customer_name = depl.inputs['customer_name'] if \
-            'customer_name' in depl.inputs else 'NO NAME'
-        created_at = depl['created_at']
-        updated_at = depl['updated_at']
-        table_cells += h_format.depl_cell.format(
-            customer_name, ID, created_at, updated_at)
+    cnt = 0
+    for instance in instances:
+        if instance['state'] == 'started':
+            deployent_id = instance.deployment_id
+            if 'customer_name' in deployments[deployent_id].inputs:
+                customer_name = \
+                    deployments[deployent_id].inputs['customer_name']
+            else:
+                customer_name = 'NO NAME'
+            created_at = deployments[deployent_id]['created_at']
+            updated_at = deployments[deployent_id]['updated_at']
+            table_cells += h_format.depl_cell.format(
+                customer_name, deployent_id, created_at, updated_at)
+            cnt += 1
+    total_sum = h_format.depl_cell.format("TOTAL", cnt, "-", "-")
+    table_cells = total_sum + table_cells
     table = h_format.depl_table.format(table_cells)
     msg += table
     h_msg = h_format.html_email.format(msg)
-
     ses = boto3.client('ses')
     destination = {'ToAddresses': [to_address]}
     ses.send_email(Source=from_address,
@@ -58,7 +76,3 @@ def main(event, context):
                            'Html': {
                                'Data': h_msg}}
                    })
-
-
-if __name__ == '__main__':
-    main(None, None)
